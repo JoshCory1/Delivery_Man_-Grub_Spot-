c98f7fede40406d3391789796be35baa485e6597
magus <accounts@keeplooking.net>
2023-02-07 13:25:49 -0700

    Update 1 on package delivery system

--- Assets/Driver/Delivery.cs
+++ Assets/Driver/Delivery.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+
     private void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ouch!");    
     }
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Package")
+        if(other.tag == "Package" && hasPackage == false)
         {
+            hasPackage = true;
             Debug.Log("Package picked up!");
         }
-        if(other.tag == "Customer")
+        if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delieverd");
+            hasPackage = false;
         }
     }
 }
