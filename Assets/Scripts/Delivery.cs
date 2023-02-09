3dc5d6be1e7a51ae535dd3d60538523358ab1bef
magus <accounts@keeplooking.net>
2023-02-09 12:54:43 -0700

    Added speed up/down mechanic

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,20 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
     [SerializeField] float timeDestroy = .5f;
     [SerializeField] float timeEnd = 2f;
     bool hasPackage = false;
+    SpriteRenderer spriteRenderer;
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ouch!");
-        // Destroy(gameObject);
-        startReload();
+        // startReload();
     }
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Package" && !hasPackage)
         {
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Debug.Log("Package picked up!");
             Destroy(other.gameObject, timeDestroy);
         }
@@ -26,6 +32,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package Delieverd");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
         
     }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 50f;
-    [SerializeField] float moveSeed = 1f;
+    [SerializeField] bool trigerSlowDown = false;
+    [SerializeField] bool trigerSpeedUp = false;
+    [SerializeField] float steerSpeed = 200f;
+    [SerializeField] float moveSeed = 15f;
+    [SerializeField] float slowSpeed = 2.5f;
+    [SerializeField] float boostSpeed = 30f;
+    float newSpeed = 1f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-
+        newSpeed = moveSeed;
     }
 
     // Update is  called once per frame
     void Update()
     {
+        speedControl();
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime; 
-        float moveAmount = Input.GetAxis("Vertical") * moveSeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * newSpeed * Time.deltaTime;
         transform.Rotate(0,0,  -steerAmount);
         transform.Translate(0,moveAmount,0);
     }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "SpeedUp")
+        {
+            trigerSlowDown = false;
+            trigerSpeedUp = true;
+            
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        trigerSpeedUp =false;
+        trigerSlowDown = true;
+        
+    }
+    void speedControl()
+    {
+        if (trigerSpeedUp)
+        {
+            newSpeed = boostSpeed;
+        }
+        if(trigerSlowDown)
+        {
+            newSpeed = slowSpeed;
+        }
+    }
+   
 }
